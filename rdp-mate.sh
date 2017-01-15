3bb7d72b398e00daba4137885d20620fb357308d
choirulanam217 <choirulanam411@gmail.com>
2017-01-15 17:57:40 +0700

    Update rdp-mate.sh

--- rdp-mate.sh
+++ rdp-mate.sh
@@ -14,11 +14,32 @@ apt-get -y upgrade
 # install ubuntu mate
 apt-get install ubuntu-mate-core ubuntu-mate-desktop -y
 
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg –I google-chrome-stable_current_amd64.deb
+
+# install wine
+apt-get install wine -y
+
 # install xrdp
 apt-get install xrdp -y
 
 # configure mate with xrdp
 echo mate-session >~/.xsession
+
+# go to root
+cd
+
+# configure xrdp config
+cd /etc/xrdp/
+mv xrdp.ini xrdp.ini.bak
+wget https://raw.githubusercontent.com/choirulanam217/linuxrdp/master/xrdp.ini
+
+# go to root
+cd
+
+# clean installer
+rm google-chrome-stable_current_amd64.deb
 
 # restart xrdp
 service xrdp restart
@@ -29,8 +50,6 @@ echo "|                            |"
 echo "|    Instalation Complete!   |"
 echo "|                            |"
 echo "=============================="
-echo ""
-echo "Server will be reboot . . ."
 
-# reboot
-reboot
+# clean installer
+rm rdp-mate.sh
